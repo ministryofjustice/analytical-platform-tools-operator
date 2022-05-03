fda7ee63c4e5921f3bebd6a1d39b2348a76fb45a
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-05-03 15:41:20 +0100

    test(tools): add controller creation test and run gofumpt

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -97,7 +97,6 @@ var _ = BeforeSuite(func() {
 		err = k8sManager.Start(ctx)
 		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
 	}()
-
 }, 60)
 
 var _ = AfterSuite(func() {
---
+++ controllers/tool_controller_test.go
@@ -0,0 +1,54 @@
+package controllers
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	toolsv1alpha1 "github.com/ministryofjustice/analytical-platform-tools-operator/api/v1alpha1"
+)
+
+var _ = Describe("Tools controller", func() {
+	BeforeEach(func() {
+		// failed test runs that don't clean up leave resources behind.
+	})
+
+	AfterEach(func() {
+		// Add any teardown steps that needs to be executed after each test
+	})
+
+	// Define utility constants for object names and testing timeouts/durations and intervals.
+	const (
+		ToolName      = "test-tool"
+		ToolNamespace = "default"
+
+		timeout  = time.Second * 10
+		duration = time.Second * 10
+		interval = time.Millisecond * 250
+	)
+
+	Context("create a tool", func() {
+		It("when a tool resource is submitted", func() {
+			// Create a tool object
+			tool := &toolsv1alpha1.Tool{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      ToolName,
+					Namespace: ToolNamespace,
+				},
+				Spec: toolsv1alpha1.ToolSpec{
+					Image:        "test-image",
+					Username:     "test-user",
+					ImageVersion: "latest",
+				},
+			}
+			By("posting a tool to the api")
+			Expect(k8sClient.Create(context.TODO(), tool)).Should(Succeed())
+			Expect(tool.Spec.Image).Should(Equal("test-image"))
+			Expect(tool.Spec.Username).Should(Equal("test-user"))
+			Expect(tool.Spec.ImageVersion).Should(Equal("latest"))
+		})
+	})
+})
