98e68d06ccbac35701a931ecb18908075271c7a9
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-05-05 18:04:47 +0100

    feat(tools): add switch and functions for airflow and rstudio
    this allows the tool parent to create both airflow and rstudio resources.

--- controllers/tool_controller.go
+++ controllers/tool_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -57,24 +58,50 @@ func (r *ToolReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 		return ctrl.Result{}, nil
 	}
 
-	if strings.ToLower(tool.Name) == "jupyterlab" {
-		log.Log.Info("Reconciling JupyterLab")
-		jupyterlab := &toolsv1alpha1.JupyterLab{}
-		err := r.Get(ctx, types.NamespacedName{Name: tool.Name, Namespace: tool.Namespace}, jupyterlab)
-		if err != nil && errors.IsNotFound(err) {
-			jupyterlabDeploy := r.createJupyterLabDeployment(tool)
-			err := r.Create(ctx, jupyterlabDeploy)
-			if err != nil {
-				log.Log.Error(err, "Failed to create JupyterLab resource")
-				return ctrl.Result{}, nil
-			}
-			return ctrl.Result{Requeue: true}, nil
-		}
-		return ctrl.Result{}, nil
+	var resource interface{}
+	switch strings.ToLower(tool.Name) {
+	case "jupyterlab":
+		log.Log.Info("Initiating JupyterLab instance for reconcile")
+		resource = &toolsv1alpha1.JupyterLab{}
+	case "rstudio":
+		log.Log.Info("Initiating Rstudio instance for reconcile")
+		resource = &toolsv1alpha1.Rstudio{}
+	case "airflow":
+		log.Log.Info("Initiating Airflow instance for reconcile")
+		resource = &toolsv1alpha1.Airflow{}
+	default:
+		log.Log.Info("Reconciling unknown tool, FAIL")
+		return ctrl.Result{}, fmt.Errorf("unknown tool %s", tool.Name)
 	}
+
+	obj, dep := r.getResource(resource, tool)
+	err := r.Get(ctx, types.NamespacedName{Name: tool.Name, Namespace: tool.Namespace}, obj)
+	if err != nil && errors.IsNotFound(err) {
+		err := r.Create(ctx, dep)
+		if err != nil {
+			log.Log.Error(err, "Failed to create tool resource")
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{Requeue: true}, nil
+	}
+
 	// TODO: Update status of the tool
 
 	return ctrl.Result{}, nil
+}
+
+// getResource extracts the resource from an interface
+func (r *ToolReconciler) getResource(obj interface{}, tool *toolsv1alpha1.Tool) (client.Object, client.Object) {
+	switch obj.(type) {
+	case *toolsv1alpha1.JupyterLab:
+		return &toolsv1alpha1.JupyterLab{}, r.createJupyterLabDeployment(tool)
+	case *toolsv1alpha1.Rstudio:
+		return &toolsv1alpha1.Rstudio{}, r.createRstudioDeployment(tool)
+	case *toolsv1alpha1.Airflow:
+		return &toolsv1alpha1.Airflow{}, r.createAirflowDeployment(tool)
+	default:
+		return nil, nil
+	}
 }
 
 func (r *ToolReconciler) createJupyterLabDeployment(tool *toolsv1alpha1.Tool) *toolsv1alpha1.JupyterLab {
@@ -93,10 +120,43 @@ func (r *ToolReconciler) createJupyterLabDeployment(tool *toolsv1alpha1.Tool) *t
 	return jupyterlab
 }
 
+func (r *ToolReconciler) createRstudioDeployment(tool *toolsv1alpha1.Tool) *toolsv1alpha1.Rstudio {
+	rstudio := &toolsv1alpha1.Rstudio{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tool.Name,
+			Namespace: tool.Namespace,
+		},
+		Spec: toolsv1alpha1.RstudioSpec{
+			Image:   tool.Spec.Image,
+			Version: tool.Spec.ImageVersion,
+		},
+	}
+	ctrl.SetControllerReference(tool, rstudio, r.Scheme)
+	return rstudio
+}
+
+func (r *ToolReconciler) createAirflowDeployment(tool *toolsv1alpha1.Tool) *toolsv1alpha1.Airflow {
+	airflow := &toolsv1alpha1.Airflow{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tool.Name,
+			Namespace: tool.Namespace,
+		},
+		Spec: toolsv1alpha1.AirflowSpec{
+			Image:   tool.Spec.Image,
+			Version: tool.Spec.ImageVersion,
+		},
+	}
+
+	ctrl.SetControllerReference(tool, airflow, r.Scheme)
+	return airflow
+}
+
 // SetupWithManager sets up the controller with the Manager.
 func (r *ToolReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&toolsv1alpha1.Tool{}).
 		Owns(&toolsv1alpha1.JupyterLab{}).
+		Owns(&toolsv1alpha1.Rstudio{}).
+		Owns(&toolsv1alpha1.Airflow{}).
 		Complete(r)
 }
